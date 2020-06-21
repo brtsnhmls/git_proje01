321202a41ba068a3884d7d62c23710fe57daeaf7
brtsnhmls <ykaraagac33@hotmail.com>
2020-06-21 21:14:34 +0200

    ikinci commit

--- src/package01/FirtClass.java
+++ src/package01/FirtClass.java
@@ -4,6 +4,7 @@ public class FirtClass {
 
 	public static void main(String[] args) {
 		System.out.println("Ilk Commit From Local Repostory");
+		System.out.println("ikinci Commit from local repository");
 	}
 
 }

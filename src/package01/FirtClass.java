131344d6ec2f6c5c6b356a1d20c8d0c17b94c6c8
brtsnhmls <ykaraagac33@hotmail.com>
2020-06-21 21:36:22 +0200

    feature branch ten ilk commit

--- src/package01/FirtClass.java
+++ src/package01/FirtClass.java
@@ -5,6 +5,9 @@ public class FirtClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk Commit From Local Repostory");
 		System.out.println("ikinci Commit from local repository");
+		
+		System.out.println(" Yeni branch olusturdum ve adi feature");
+	
 	}
 
 }
